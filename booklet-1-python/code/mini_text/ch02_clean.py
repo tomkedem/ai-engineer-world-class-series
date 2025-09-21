3f4050ee36c974be31427af68f4161100d4248a2
Tomer Kedem <tompha1@hotmail.com>
2025-09-21 10:07:04 +0300

    Second init

--- booklet-1-python/code/mini_text/ch02_clean.py
+++
@@ -1,9 +0,0 @@
-def clean_text(text: str) -> str:
-    """
-    מנקה טקסט: מסיר רווחים מיותרים ומחזיר lowercase.
-    """
-    return " ".join(text.split()).lower()
-
-if __name__ == "__main__":
-    sample = "   פייתון   היא   שפה  נהדרת   "
-    print(clean_text(sample))
